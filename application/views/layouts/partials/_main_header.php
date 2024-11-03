1189287af2c4d7250c28b7fa5716760210d752ff
hasakimix <mickoaira@gmail.com>
2024-11-03 19:46:32 +0800

    Static notification added

--- application/views/layouts/partials/_main_header.php
+++ application/views/layouts/partials/_main_header.php
@@ -16,7 +16,26 @@
     </a>
     <i class="bi bi-grid"></i>
     <i class='bx bx-sun' id="darkLight"></i>
-    <i class='bx bx-bell'></i>
+    
+		<i class='bx bx-bell' onclick="toggleNotification()"></i>
+		<div id="notificationDropdown" class="notification-dropdown">
+			<p class="notification-title">Notifications</p>
+			
+			<!-- Example notifs -->
+			<div class="notification-item">
+				<div class="notification-text">
+					<span class="notification-message">New module available: "Introduction to Python"</span>
+					<span class="notification-timestamp">Just now</span>
+				</div>
+			</div>
+			
+			<div class="notification-item">
+				<div class="notification-text">
+					<span class="notification-message">Assignment deadline approaching: "JavaScript Basics"</span>
+					<span class="notification-timestamp">2 hours ago</span>
+				</div>
+			</div>
+		</div>
 
 		<div class="dropdown">
 			<img src="<?= base_url("/assets/img/profileicon.png") ?>" onclick="toggleDropdown()" class="dropbtn">
