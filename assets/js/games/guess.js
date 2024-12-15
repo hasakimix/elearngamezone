e321174a0310ac4158fe6398eecc234444200c15
hasakimix <mickoaira@gmail.com>
2024-12-15 19:47:09 +0800

    rara

--- assets/js/games/guess.js
+++ assets/js/games/guess.js
@@ -112,7 +112,7 @@ document.addEventListener("keydown", () => typingInput.focus());
 
 randomWord();
 
-// Event listener for the back button tests
+// Event listener for the back button rara
 backBtn.addEventListener("click", () => {
   window.location.href = BACK_URL; // Change this to the actual URL of your game selection page
 });
