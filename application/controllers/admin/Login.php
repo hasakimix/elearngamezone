d9bc8a4a3d470a7ceb38578abff3c6c5b69daac1
hasakimix <mickoaira@gmail.com>
2025-01-18 21:20:20 +0800

    check

--- application/controllers/admin/Login.php
+++ application/controllers/admin/Login.php
@@ -9,14 +9,47 @@ class Login extends MY_Controller {
 	public function __construct()
     {
         parent::__construct();
+        $this->load->library('session');
     }
-	
+
 	public function index()
 	{
-        $data = &$this->data;
-        $data['final_view'] = $this->_template.'_auth_template';
-        $data['primary_view'] = $this->_primary_view.'login';
-		$this->load->view($data['final_view'], $data);   
-	}
-}
+        if ($this->input->post()) {
+            $email = $this->input->post('email');
+            $password = $this->input->post('password');
 
+            if ($email === 'admin@example.com' && $password === 'password') {
+                $this->build_session(['id' => 1, 'email' => $email]);
+                redirect(base_url('/home'));
+            } else {
+                $this->session->set_flashdata('error', 'Invalid Credentials');
+                redirect(base_url('/'));
+            }
+        } else {
+            $this->load->view('admin/login'); 
+        }
+	}
+
+	private function build_session($data)
+    {
+        $this->session->set_userdata([
+            'id' => $data['id'],
+            'email' => $data['email'],
+			'is_admin' => false,
+            'logged_in' => true
+        ]);
+        return true;
+    }
+
+    public function end_sessions()
+    {
+        $this->session->sess_destroy();
+
+        if (isset($_COOKIE['userUID'])) {
+            unset($_COOKIE['userUID']);
+            setcookie('userUID', '', 1, '/'); 
+        }
+
+        redirect(base_url('/'));
+    }
+}
